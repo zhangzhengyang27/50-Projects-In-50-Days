e8813b2f6140fb95d8808980732d26e8192470d3
zhangzhengyang <1074385735@qq.com>
2022-08-01 18:59:36 +0800

    background-slider

---
+++ 18-background-slider/script.js
@@ -0,0 +1,42 @@
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length -1){
+        activeSlide = 0
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+
+    if(activeSlide < 0){
+        activeSlide = slides.length - 1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+// 设置背景的图片
+function setBgToBody(){
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+// 设置高亮的图片
+function setActiveSlide(){
+    slides.forEach(slide => slide.classList.remove('active'))
+
+    slides[activeSlide].classList.add('active')
+}
