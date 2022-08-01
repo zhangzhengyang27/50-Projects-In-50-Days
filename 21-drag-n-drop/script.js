6b8f594166b5bbb8e3953d58e230da527eadad7a
zhangzhengyang <1074385735@qq.com>
2022-08-01 20:31:12 +0800

    Drag N Drop

---
+++ 21-drag-n-drop/script.js
@@ -0,0 +1,41 @@
+const fill = document.querySelector('.fill')
+const empties = document.querySelectorAll('.empty')
+
+fill.addEventListener('dragstart', dragStart)
+fill.addEventListener('dragend', dragEnd)
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+    this.className += ' hold';
+    // 这个不能删除，确保拖动的时候，原来的框没有残影
+    setTimeout(() => this.className = 'invisible', 0)
+
+}
+
+function dragEnd() {
+    this.className = 'fill'
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'empty'
+}
+
+function dragDrop() {
+    this.className = 'empty'
+    this.append(fill)
+}
