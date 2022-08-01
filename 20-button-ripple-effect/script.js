bfc7b7af16df6143ddf3be235b901ea33096181c
zhangzhengyang <1074385735@qq.com>
2022-08-01 20:22:59 +0800

    Button Ripple Effec

---
+++ 20-button-ripple-effect/script.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('.ripple')
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        // 确定点击的位置，即动画开始的位置
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(()=> circle.remove(), 500)
+    })
+})
